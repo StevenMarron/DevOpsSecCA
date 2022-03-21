1df352cf778fad5c5b3e65ab42d16620409253ce
StevenMarron <stevomarron@hotmail.com>
2022-03-21 15:32:19 +0000

    config update, deploy.sh added

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+sudo apt update && sudo apt install nodejs npm
+sudo npm install -g pm2
+pm2 stop example_app
+cd DevOpsCA/
+npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+pm2 start ./bin/www --name example_app
